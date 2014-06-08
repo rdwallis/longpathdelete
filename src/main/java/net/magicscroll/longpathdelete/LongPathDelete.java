22f11ca6b2ece76b4117da857f2dca2d9e519bda
Richard Wallis <rdwallis@gmail.com>
2014-06-08 09:31:27 +0200

    check path length before rename

--- src/main/java/net/magicscroll/longpathdelete/LongPathDelete.java
+++ src/main/java/net/magicscroll/longpathdelete/LongPathDelete.java
@@ -4,22 +4,22 @@ import java.io.File;
 
 public class LongPathDelete {
 
-	public static void main(String[] args) {
-		
-		String path = args[0];
-		System.out.println("Processing: " + path);
-		File folder = new File(path);
-		File[] listOfFiles = folder.listFiles();
+    public static void main(final String[] args) {
 
-		for (int i = 0; i < listOfFiles.length; i++) {
-			File file = listOfFiles[i];
-			
-			if (file.lastModified() < System.currentTimeMillis() - 86400000) {
-				System.out.println("Will delete " + file.toString());
-				file.renameTo(new File(path + "/" + i + "d.d"));
-			}
-		}
+        final String path = args[0];
+        System.out.println("Processing: " + path);
+        final File folder = new File(path);
+        final File[] listOfFiles = folder.listFiles();
 
-	}
+        for (int i = 0; i < listOfFiles.length; i++) {
+            final File file = listOfFiles[i];
+
+            if (file.getName().length() > 120 && file.lastModified() < System.currentTimeMillis() - 86400000) {
+                System.out.println("Will delete " + file.toString());
+                file.renameTo(new File(path + "/" + i + "d.d"));
+            }
+        }
+
+    }
 
 }
